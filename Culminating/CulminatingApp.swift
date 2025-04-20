d0f19e3af7cfb36e4fa2bb5750f78894711bbc3f
Marcus Li <mmli@lcs.on.ca>
2025-04-20 16:40:14 -0400

    Initial Commit

---
+++ Culminating/CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingApp.swift
+//  Culminating
+//
+//  Created by Marcus Li on 2025-04-20.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
